47dc7676ec54c992a7ec220a3ce335e80226debf
Puyan Wei <puyanwei@gmail.com>
2018-03-17 18:08:05 +0000

    rolled back for-of loops they didn't work lol

--- src/interface.js
+++ src/interface.js
@@ -4,12 +4,13 @@ $(window).on("load", event => {
   function updateBoard() {
     let cells = $("td");
     let arr = [];
-    for (let row of rows) {
+    for (let rows = 0; rows < game.board.length; rows++) {
       for (let cols = 0; cols < game.board[rows].length; cols++) {
         arr.push(game.board[rows][cols]);
       }
     }
-    for (let cell of cells) {
+
+    for (let i = 0; i < cells.length; i++) {
       cells[i].innerText = arr[i];
     }
   }
