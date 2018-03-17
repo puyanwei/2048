5e1327ea09f1f9455cdf65a0664c29eeeffe066d
Puyan Wei <puyanwei@gmail.com>
2018-03-17 18:03:34 +0000

    refactored some for loops to use ES6's new for-of loop

--- src/interface.js
+++ src/interface.js
@@ -4,12 +4,12 @@ $(window).on("load", event => {
   function updateBoard() {
     let cells = $("td");
     let arr = [];
-    for (let rows = 0; rows < game.board.length; rows++) {
+    for (let row of rows) {
       for (let cols = 0; cols < game.board[rows].length; cols++) {
         arr.push(game.board[rows][cols]);
       }
     }
-    for (let i = 0; i < cells.length; i++) {
+    for (let cell of cells) {
       cells[i].innerText = arr[i];
     }
   }
