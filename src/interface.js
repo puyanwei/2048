debf67304661095d6793bb2ac474100037555dd6
Puyan Wei <puyanwei@gmail.com>
2018-03-18 15:28:55 +0000

    refactored to use slideAndCombine in slideRight function in interface.js

--- src/interface.js
+++ src/interface.js
@@ -7,9 +7,7 @@ $(window).on("load", event => {
 
   function slideRight() {
     for (let i = 0; i < 4; i++) {
-      game.board[i] = game.slide(game.board[i]);
-      game.board[i] = game.combine(game.board[i]);
-      game.board[i] = game.slide(game.board[i]);
+      game.board[i] = game.slideAndCombine(game.board[i]);
     }
     generateNumber();
     updateBoardToPage();
