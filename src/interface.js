482f182e4caf2b1c7373a0df504b701cfa1cb2e8
Puyan Wei <puyanwei@gmail.com>
2018-03-25 18:12:57 +0100

    renamed #rotateClockwise to #flipDiagonally to explain more accurately what the function is doing

--- src/interface.js
+++ src/interface.js
@@ -1,91 +1,91 @@
 $(window).on("load", event => {
-  let game = new Game();
+    let game = new Game();
 
-  $(document).keydown(function(e) {
-    let prevBoard = game.copyBoard(game.board);
-    switch (e.which) {
-      case 37: // left
-        game.flip(game.board);
-        slideRight();
-        game.flip(game.board);
-        break;
+    $(document).keydown(function(e) {
+        let prevBoard = game.copyBoard(game.board);
+        switch (e.which) {
+            case 37: // left
+                game.flip(game.board);
+                slideRight();
+                game.flip(game.board);
+                break;
 
-      case 38: // up
-        game.flip(game.board);
-        game.rotate(game.board);
-        slideRight();
-        game.flip(game.board);
-        game.rotate(game.board);
-        break;
+            case 38: // up
+                game.flip(game.board);
+                game.flipDiagonally(game.board);
+                slideRight();
+                game.flip(game.board);
+                game.flipDiagonally(game.board);
+                break;
 
-      case 39: // right
-        slideRight();
-        break;
+            case 39: // right
+                slideRight();
+                break;
 
-      case 40: // down
-        game.rotate(game.board);
-        slideRight();
-        game.rotate(game.board);
-        break;
+            case 40: // down
+                game.flipDiagonally(game.board);
+                slideRight();
+                game.flipDiagonally(game.board);
+                break;
 
-      default:
-        return; // exit this handler for other keys
+            default:
+                return; // exit this handler for other keys
+        }
+        e.preventDefault();
+        isGameOver(prevBoard);
+    });
+
+    $("#restart").on("click", () => {
+        game.reset();
+        newGame();
+    });
+
+    function slideRight() {
+        for (let i = 0; i < 4; i++) {
+            game.board[i] = game.slideAndCombine(game.board[i]);
+        }
     }
-    e.preventDefault();
-    isGameOver(prevBoard);
-  });
 
-  $("#restart").on("click", () => {
-    game.reset();
+    function isGameOver(prevBoard) {
+        let changed = game.isChange(prevBoard, game.board);
+        if (changed) {
+            generateNumber();
+            updateBoardToPage();
+        } else {
+            console.log("game over");
+        }
+    }
+
+    function updateBoardToPage() {
+        let cells = $("td");
+        let arr = [];
+        for (let rows = 0; rows < game.board.length; rows++) {
+            for (let cols = 0; cols < game.board[rows].length; cols++) {
+                arr.push(game.board[rows][cols]);
+            }
+        }
+        for (let i = 0; i < cells.length; i++) {
+            cells[i].innerText = arr[i];
+        }
+    }
+
+    function generateNumber() {
+        let row = game.randomiser(4);
+        let cols = game.randomiser(4);
+        let number = game.randomiser(2);
+        number === 0 ? (number = 2) : (number = 4);
+        if (game.isEmpty(game.board[row][cols])) {
+            game.addNumberToBoard(number, row, cols);
+        } else {
+            generateNumber();
+        }
+    }
+
+    function newGame() {
+        generateNumber();
+        generateNumber();
+        updateBoardToPage();
+    }
+
     newGame();
-  });
-
-  function slideRight() {
-    for (let i = 0; i < 4; i++) {
-      game.board[i] = game.slideAndCombine(game.board[i]);
-    }
-  }
-
-  function isGameOver(prevBoard) {
-    let changed = game.isChange(prevBoard, game.board);
-    if (changed) {
-      generateNumber();
-      updateBoardToPage();
-    } else {
-      console.log("game over");
-    }
-  }
-
-  function updateBoardToPage() {
-    let cells = $("td");
-    let arr = [];
-    for (let rows = 0; rows < game.board.length; rows++) {
-      for (let cols = 0; cols < game.board[rows].length; cols++) {
-        arr.push(game.board[rows][cols]);
-      }
-    }
-    for (let i = 0; i < cells.length; i++) {
-      cells[i].innerText = arr[i];
-    }
-  }
-
-  function generateNumber() {
-    let row = game.randomiser(4);
-    let cols = game.randomiser(4);
-    let number = game.randomiser(2);
-    number === 0 ? (number = 2) : (number = 4);
-    if (game.isEmpty(game.board[row][cols])) {
-      game.addNumberToBoard(number, row, cols);
-    } else {
-      generateNumber();
-    }
-  }
-
-  function newGame() {
-    generateNumber();
-    generateNumber();
-    updateBoardToPage();
-  }
-
-  newGame();
 });
