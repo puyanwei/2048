b3dd94033e48ae8d17787f441ec2949f605709d1
Puyan Wei <puyanwei@gmail.com>
2018-04-02 12:27:21 +0100

    wrote test for adding combined numbers to score, might not be good to put in #combine, will refactor later

--- test/game-test.js
+++ test/game-test.js
@@ -52,6 +52,11 @@ describe('Game', () => {
             expect(game.combine(['', '', 2, 2])).toEqual(['', '', '', 4]);
             expect(game.combine(['', '', 2, 4])).toEqual(['', '', 2, 4]);
         });
+        it('adds the combined numbers to the score', () => {
+            this.score = 0;
+            game.combine(['', '', 4, 4]);
+            expect(game.score).toEqual(8);
+        });
     });
 
     describe('#slideRight', () => {
